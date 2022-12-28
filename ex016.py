e41b35f0472aba11197391b541a8d6ca3fb67fc9
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-28 14:11:30 -0300

    Quebrando um número com duas formas diferentes
    Programa que lê um número Real qualquer pelo teclado e mostra na tela a sua porção Inteira

---
+++ ex016.py
@@ -0,0 +1,7 @@
+import math
+
+valor = float(input("Digite um valor: "))
+
+print(f"O valor digitador foi {valor} e a sua porção inteira é {math.trunc(valor)}")
+
+print(f"O valor digitador foi {valor} e a sua porção inteira é {int(valor)}")
