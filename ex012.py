d5b3a9db0a5e5683e28ca7a5cc727faaaf676993
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-28 13:32:10 -0300

    Calculando descontos
    Programa que lê o preço de um produto e mostra seu novo preço, com 5% de desconto.

--- ex012.py
+++ ex012.py
@@ -2,4 +2,5 @@ preco_inicial = float(input("Qual é o preço do produto? "))
 taxa_desconto = 5
 preco_final = preco_inicial * (1 - taxa_desconto/100)
 
+
 print(f"O produto que custava R${preco_inicial:.2f}, na promoção com um desconto de {taxa_desconto}% vai custar R${preco_final:.2f}.")
