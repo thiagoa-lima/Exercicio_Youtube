9d5599f16c169d2378cab0df785fb72ca601c5b9
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-28 10:52:13 -0300

    Adicionando um novo exercicio para calcular a media entre dois números
    media = (nota_2 + nota_1) / 2

---
+++ ex007.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Digite a primeira nota do aluno: "))
+nota_2 = float(input("Digite a segunda nota do aluno: "))
+
+media = (nota_2 + nota_1) / 2
+
+print(f"A média entre {nota_1:.1f} e {nota_2:.1f} é {media:.1f}")
