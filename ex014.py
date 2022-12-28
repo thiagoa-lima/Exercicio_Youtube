75f1a3ee26ed5e26192bb8392a13d28a1f1eaf20
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-28 13:48:13 -0300

    Conversor de temperaturas
    Programa que converte uma temperatura de Celsius para Fahrenheit.

---
+++ ex014.py
@@ -0,0 +1,4 @@
+temp_celsius = float(input("Informe a temperatura em Celsius: "))
+temp_fahrenheit = temp_celsius * 1.8 + 32
+
+print(f"A temperatura de {temp_celsius:.2f}ºC corresponde a {temp_fahrenheit:.2f}ºF!")
