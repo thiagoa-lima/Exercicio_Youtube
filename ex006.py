e77d4a814f06114104ebd01a312a417726741629
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-28 10:29:56 -0300

    Adicionando um novo exercicio com operações matemáticas.
    Dobro, triplo e raiz quadrada.

---
+++ ex006.py
@@ -0,0 +1,11 @@
+import math
+
+numero_1 = int(input("Digite um número: "))
+
+dobro = numero_1*2
+triplo = numero_1*3
+raiz = math.sqrt(numero_1)
+
+print(f"O dobro de {numero_1} é {dobro}")
+print(f"O triplo de {numero_1} é {triplo}")
+print(f"A raiz quadrada de {numero_1} é {raiz:.2f}")
