e0684bba66497660ac5d70146139a4e31d01b0c1
thiagoandradelima <thiagog_12@hotmail.com>
2022-12-29 20:19:49 -0300

    Maior e menor valores
    Faça um programa que leia três números e mostre qual é o maior e qual é o menor.

---
+++ ex033.py
@@ -0,0 +1,12 @@
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+c = int(input("Terceiro valor: "))
+
+lista = [a, b, c]
+
+menor = min(lista)
+maior = max(lista)
+
+print(f"O menor valor digitado foi {menor}.")
+print(f"O maior valor digitado foi {maior}.")
+
