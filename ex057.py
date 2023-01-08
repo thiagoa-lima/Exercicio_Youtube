294b3990a5fbc2f7df18c0d8229dd08eda0899d9
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-08 12:41:02 -0300

    Validação de Dados
    Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.

--- ex057.py
+++ ex057.py
@@ -1,6 +1,6 @@
 
-sexo = str(input("Informe seu sexo: [M/F] ").upper())
+sexo = str(input("Informe seu sexo: [M/F] ").upper().strip())
 
 while sexo not in ("M", "F"):
-    sexo = str(input("Dados inválidos. Por favor, informe seu sexo: ").upper())
+    sexo = str(input("Dados inválidos. Por favor, informe seu sexo: ").upper().strip())
 
