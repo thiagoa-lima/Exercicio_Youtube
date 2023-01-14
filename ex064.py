f3c834b0cdaafbb78a48654889e8d819bc33c464
thiagoandradelima <thiagog_12@hotmail.com>
2023-01-14 09:51:42 -0300

    pequeno ajuste nas linhas de código

--- ex064.py
+++ ex064.py
@@ -6,5 +6,6 @@ while numero != 999:
     soma += numero
     cont += 1
     numero = int(input("Digite um número qualquer [999 para parar]: "))
+    print(cont, numero, soma)
 
 print(f"Você digitou {cont} e a soma entre eles foi {soma}")
